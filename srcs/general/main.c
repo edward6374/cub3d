6dc507f2f6a83680260b7f149eb2ebc4709bfafa
Nacho Mota 🚀 <nacho.mota@gmail.com>
2023-11-08 16:53:53 +0100

    check rgb OK

--- srcs/general/main.c
+++ srcs/general/main.c
@@ -6,7 +6,7 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 18:09:01 by vduchi            #+#    #+#             */
-/*   Updated: 2023/11/08 16:26:26 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2023/11/08 16:53:41 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,7 +91,7 @@ int	main(int argc, char *argv[])
 	ft_bzero(&cube, sizeof(t_cube));
 	if (check_file(argc, argv[1]) || check_params(&cube, argv[1]))
 		return (EXIT_FAILURE);
-
+	//=========================================================================
 	int i = 0;
 	while (cube.params.textures[i])
 	{
@@ -100,6 +100,15 @@ int	main(int argc, char *argv[])
 		i++;
 	}
 
+	i = 0;
+	while (i < 2)
+	{
+		ft_printf("r: %d\n", cube.params.colors[i].r);
+		ft_printf("g: %d\n", cube.params.colors[i].g);
+		ft_printf("b: %d\n", cube.params.colors[i].b);
+		i++;
+	}
+	//=========================================================================
 	init_func(&cube);
 
 	ft_printf("Value: %d\n", init_mlx(&cube));
--- srcs/map/check_map.c
+++ srcs/map/check_map.c
@@ -6,36 +6,51 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 14:20:22 by vduchi            #+#    #+#             */
-/*   Updated: 2023/11/08 16:27:43 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2023/11/08 16:53:13 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+#include "error.h"
+#include "libft.h"
 #include <fcntl.h>
 
 //	TODO
 
 // Comprobar NO,SO,WE,EA,F,C este
 // Comprobar que la ruta sea correcta y este la textura
-// comprobar que sea un valor valido para RGB entre 0 y 255
 // Leer el mapa y todal de filas sera el Primer Puntero
 // Leer el mapa, la fila mas larga sera el total del Doble puntero
+
+void check_rgb(int r, int g, int b)
+{
+	if ((r > 255 || r < 0) || (g > 255 || g < 0) || (b > 255 || b < 0))
+	{
+		ft_message(DANGER, "Error\nThe color is not within RBG.");
+		exit(EXIT_FAILURE);
+	}
+}
 
 void init_color(t_color *color, char *split)
 {
 	char **rgb;
+	int r;
+	int g;
+	int b;
 
 	rgb = ft_split(split, ',');
-	color->r = ft_atoi(rgb[0]);
-	color->g = ft_atoi(rgb[1]);
-	color->b = ft_atoi(rgb[2]);
+	r = ft_atoi(rgb[0]);
+	g = ft_atoi(rgb[1]);
+	b = ft_atoi(rgb[2]);
+	check_rgb(r, g, b);
+	color->r = r;
+	color->g = g;
+	color->b = b;
 	free(rgb);
 }
 
-int init_texture(char **params, char **textures)
+void init_texture(char **params, char **textures)
 {
-	// textures = malloc(sizeof(char *) * 5);
-	// // return (EXIT_FAILURE);
 	if (!ft_strncmp(params[0], "NO", 2))
 		textures[NO] = ft_strdup(params[1]);
 	else if (!ft_strncmp(params[0], "SO", 2))
@@ -44,40 +59,25 @@ int init_texture(char **params, char **textures)
 		textures[WE] = ft_strdup(params[1]);
 	else if (!ft_strncmp(params[0], "EA", 2))
 		textures[EA] = ft_strdup(params[1]);
-
-	return (0);
 }
 
 int check_params(t_cube *cube, char *file)
 {
 	char **data;
 	char **split;
-	int err;
-
-	err = 0;
+	int i;
 
 	data = ft_file_to_dptr(file, 1);
 	if (!(cube->params.textures = malloc(sizeof(char *) * 5)) || !data)
 		return (EXIT_FAILURE);
-	int i = 0;
+	i = 0;
 	if (data)
 	{
 		while (data[i])
 		{
 			split = ft_split(data[i], ' ');
-
 			init_texture(split, cube->params.textures);
-
-			// if (!ft_strncmp(split[0], "NO", 2))
-			// 	cube->params.textures[NO] =t_strdup(split[1]);
-			// else if (!ft_strncmp(split[0], "SO", 2))
-			// 	cube->params.textures[SO] = ft_strdup(split[1]);
-			// else if (!ft_strncmp(split[0], "WE", 2))
-			// 	cube->params.textures[WE] = ft_strdup(split[1]);
-			// else if (!ft_strncmp(split[0], "EA", 2))
-			// 	cube->params.textures[EA] = ft_strdup(split[1]);
 			if (!ft_strncmp(split[0], "C", 1))
-				// else if (!ft_strncmp(split[0], "C", 1))
 				init_color(&cube->params.colors[C], split[1]);
 			else if (!ft_strncmp(split[0], "F", 1))
 				init_color(&cube->params.colors[F], split[1]);
@@ -87,13 +87,5 @@ int check_params(t_cube *cube, char *file)
 	}
 	cube->params.textures[4] = NULL;
 	free(data);
-
-	// i = 0;
-	// while (cube->params.textures[i])
-	// {
-
-	// 	ft_printf("%s\n", cube->params.textures[i]);
-	// 	i++;
-	// }
-	return (err);
+	return (0);
 }
